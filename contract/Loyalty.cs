005e9cd412945faefbd88d40f190bd6216de5d35
Harry <harrypierson@ngd.neo.org>
2020-09-14 10:19:34 -0700

    Initial commit

---
+++ contract/Loyalty.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Neo.SmartContract.Framework;
+
+namespace LoyaltySample
+{
+    [ManifestExtra("Author", "Harry Pierson")]
+    [ManifestExtra("Email", "hpierson@ngd.neo.org")]
+    [ManifestExtra("Description", "This is a TTF Loyalty example")]
+    [Features(ContractFeatures.HasStorage | ContractFeatures.Payable)]
+    public partial class LoyaltyToken : SmartContract
+    {
+    }
+}
