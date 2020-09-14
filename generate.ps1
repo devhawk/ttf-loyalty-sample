a046404403ad4c836633a4e9e32cf578d42de24c
Harry <harrypierson@ngd.neo.org>
2020-09-14 13:19:00 -0700

    mint/xfer/burn

--- generate.ps1
+++ generate.ps1
@@ -1,3 +1,11 @@
 dotnet build ./contract
 dotnet nxp3 reset -f
-dotnet nxp3 contract deploy ./contract/bin/Debug/netstandard2.1/safe-purchase.nef genesis
+dotnet nxp3 transfer gas 1000 genesis alice
+dotnet nxp3 contract deploy ./contract/bin/Debug/netstandard2.1/Loyalty.nef genesis
+dotnet nxp3 checkpoint create checkpoints/mint -f
+dotnet nxp3 contract invoke ./invoke-files/mint.neo-invoke.json alice
+dotnet nxp3 checkpoint create checkpoints/transfer -f
+dotnet nxp3 contract invoke ./invoke-files/transfer.neo-invoke.json alice
+dotnet nxp3 checkpoint create checkpoints/burn -f
+dotnet nxp3 contract invoke ./invoke-files/burn.neo-invoke.json alice
+dotnet nxp3 checkpoint create checkpoints/after-burn -f
